18de3791fad1a0951d32718bf392d0ea6c05b010
Vaughn Seekamp <vseekamp@gmail.com>
2023-04-12 11:00:23 -0700

    working through routes

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,7 +1,14 @@
 const { BadRequestError } = require("../expressError");
 
 // THIS NEEDS SOME GREAT DOCUMENTATION.
-
+/** Takes in an object to update and an object that translates JS to SQL
+ * transforms data to update into an array of columns and parameterized variables
+ * returns string with comma seperated data and
+ *  another array of values from the original input
+ *  that will become the new field values in the database
+ *
+ * throws error if no data is provided
+  */
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
